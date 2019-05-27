840ca4c62bbc111c12c8b2c9662e5873e58d6139
Newairs <gabriel_arantes18@hotmail.com>
2019-05-27 07:53:16 -0300

    Event card created, added placeholders

---
+++ ui/src/EventDeck/EventCard.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+function EventCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="Evento" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Evento A"
+        subheader="Data de criação"
+      />
+      <CardMedia
+        className={classes.media}
+        image="/static/images/cards/placeholder.jpg"
+        title="Place holder"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Placeholder
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="Show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+            Placeholder
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
+export default EventCard;
--- ui/src/EventDeck/EventDeck.js
+++ ui/src/EventDeck/EventDeck.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import EventCard from '/src/EventDeck';
 
-const  EventDeck = () => <div>
-	Card 
+const  EventDeck = () => <div id="deck">
+	<div class="card"></div>
+	EventCard.EventCard();
 </div>
 
 export default EventDeck;
